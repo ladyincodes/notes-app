7f17ce90b29595ed99d0a1927e4c5a9b69f9469e
Atefeh <111862613+ladyincodes@users.noreply.github.com>
2026-08-04 16:50:53 -0500

    Setup routers

--- notes-ui/src/App.jsx
+++ notes-ui/src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotesList from "./pages/NotesList";
+import CreateNote from "./pages/CreateNote";
+import EditNote from "./pages/EditNote";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NotesList />} />
+        <Route path="/create" element={<CreateNote />} />
+        <Route path="/edit" element={<EditNote />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
