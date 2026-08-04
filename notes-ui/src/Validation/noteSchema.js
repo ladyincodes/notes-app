41b5bcd40ecbe9b7185e1e176251eba6fdb58c14
Atefeh <111862613+ladyincodes@users.noreply.github.com>
2026-08-04 18:20:18 -0500

    fixed the typo

--- notes-ui/src/Validation/noteSchema.js
+++ notes-ui/src/Validation/noteSchema.js
@@ -2,5 +2,5 @@ import * as Yup from "yup";
 
 export const noteSchema = Yup.object({
   title: Yup.string().required("Title is required"),
-  constent: Yup.string().required("Content is required"),
+  content: Yup.string().required("Content is required"),
 });
